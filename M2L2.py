4c8c203bdbb0c4868ba7b388fd79a54b9d47e76e
FDvorak1979 <f.dvorak2@students.clark.edu>
2020-04-29 12:03:29 -0700

    Mod 2 Lab 2

--- M2L2.py
+++ M2L2.py
@@ -1,8 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
-<assignment/lab description
+Frank Dvorak
+Mod 2 Lab 2
 """
 
 """ IPO template
@@ -20,4 +19,12 @@ def main():
     print(type(myInt))
     r = 3
     v = (4/3)*pi*r**3
+    print()
+    
+    from math import*
+    sum = 0
+    for i in range(5)
+    inputValue = eval(input("Enter a numberL: "))
+    sum = sum + inputValue
+    print("sum:", sum)
 main()    
